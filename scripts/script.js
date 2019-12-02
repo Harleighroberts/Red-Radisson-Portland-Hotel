4e3ae1ab9eeb4a0f4e305388b2df62fbf83dc6cb
Harleighroberts <55995148+Harleighroberts@users.noreply.github.com>
2019-12-02 10:34:51 -0800

    check in

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+// your script file for adding your own jquery
+
+// Your Code from here on down. Don't delete that line above!
+  
+
+// 4. Enable your slideshow
+// Here's the basic version that uses all default settings
+// $('.slideshow').square1();
+
+
+// You can adjust the optional settings to control how square1 works by using code like this instead:
+
+
+
+
+$('.slideshow-2').square1({
+	animation: 'slide',
+	transition_time: 3000,
+	prev_next_nav: 'hover',
+	dots_nav: 'hover'
+});
+
+$('.slideshow-3').square1({
+	animation: 'slide',
+	transition_time: 2000,
+	prev_next_nav: 'hover',
+	dots_nav: 'hover'
+});
+
+
+$('.slideshow-4').square1({
+	animation: 'slide',
+	transition_time: 3000,
+	prev_next_nav: 'hover',
+	dots_nav: 'hover'
+});
+
+
+
+
+
+
+
+
+
+
+// End of Your Code . Don't delete that line below!!
